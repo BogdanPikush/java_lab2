f99205843a0cca1626a765bfba7c394a1176dbe8
BogdanPikush <120464354+BogdanPikush@users.noreply.github.com>
2023-10-19 23:51:32 +0300

    update class Patron. added method generateUniqueID

--- src/main/java/org/example/Patron.java
+++ src/main/java/org/example/Patron.java
@@ -2,15 +2,16 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class Patron {
     private String name;
     private String ID;
     private List<Item> borrowedItems;
 
-    public Patron(String name, String ID) {
+    public Patron(String name) {
         this.name = name;
-        this.ID = ID;
+        this.ID = generateUniqueID();
         borrowedItems = new ArrayList<>();
     }
 
@@ -38,5 +39,16 @@ public class Patron {
             item.returnItem();
             borrowedItems.remove(item);
         }
+    }
+
+    private static int lastUsedID = 0;
+
+    private String generateUniqueID() {
+
+        String uuid = UUID.randomUUID().toString();
+
+        String uniqueID = "ID" + uuid.substring(0, 8) + lastUsedID;
+        lastUsedID++;
+        return uniqueID;
     }
 }
